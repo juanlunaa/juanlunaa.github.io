3f4e985bdcefa3f82d4182e174ea930eac482ca2
juanlunaa <jp3165239@gmail.com>
2023-01-31 18:49:54 -0500

    Actualización del codigo

--- src/code.js
+++ src/code.js
@@ -1,23 +1,25 @@
-window.addEventListener("load", listener)
+window.addEventListener("load", starCalculator)
+let discount
+let bill
+let numberPeople
 
-//deselectAmountTip()
-
-function deselectAmountTip(){
-    let five = document.getElementById("five")
-    let ten = document.getElementById("ten")
-    let fifteen = document.getElementById("fifteen")
-    let twentyFive = document.getElementById("twenty-five")
-    let fifty = document.getElementById("fifty")
-    let custom = document.getElementById("custom")
-
-    console.log("Hola")
-
-    if(fifty.checked){
-        console.log("Hola")
-    }
+function starCalculator(){
+    getBill()
+    discountManager()
+    getNumberPeople()
 }
 
-function listener(){
+function getBill(){
+    let inputBill = document.getElementById("bill")
+    inputBill.addEventListener("keypress", function(){bill = inputBill.value})
+}
+
+function getNumberPeople(){
+    let inputNumberPeople = document.getElementById("number-people")
+    inputNumberPeople.addEventListener("keypress", function(){numberPeople = inputNumberPeople.value})
+}
+
+function discountManager(){
     let five = document.getElementById("five")
     let ten = document.getElementById("ten")
     let fifteen = document.getElementById("fifteen")
@@ -26,7 +28,17 @@ function listener(){
     let custom = document.getElementById("custom")
 
     custom.addEventListener("focus", function(){
-        console.log("Hello world")
         document.getElementById("inputs").reset()
     })
+
+    custom.addEventListener("keypress", function(){
+        discount = custom.value
+        console.log(discount)
+    })
+
+    five.addEventListener("click", function(){discount = 5})
+    ten.addEventListener("click", function(){discount = 10})
+    fifteen.addEventListener("click", function(){discount = 15})
+    twentyFive.addEventListener("click", function(){discount = 25})
+    fifty.addEventListener("click", function(){discount = 50})
 }
